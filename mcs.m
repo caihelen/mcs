44d7fee4ec9582b766c3cb0575d09541c27ae5b2
caihelen <helen.cai@yale.edu>
2019-05-31 17:50:24 -0600

    add information struct I

--- mcs.m
+++ mcs.m
@@ -6,23 +6,53 @@
 % The global minimum is known to be at (0,0) where f = 0
 
 rng(5); % Set seed
-G = 0; % Generation number
+I.G = 1; % Generation number
+I.Pa = 0.25; % fraction of abandoned nests
+I.A = 1; % Maximum Levy step size
+I.B = 1.1; % Power law index, 1 < B < 2
 
 % Initialize nests in population
-n_nests = 10;
-n_params = 2; % = number of positions to optimize
-nests = rand(n_nests, n_params); % Each row is a nest
+I.n_nests = 20;
+I.n_params = 2; % = number of positions to optimize
+nests = rand(I.n_nests, I.n_params); % Each row is a nest
 
 % Calculate all fitness scores in a population
-scores = zeros(1, n_nests);
-for i = 1:n_nests
+scores = zeros(I.n_nests, 1);
+for i = 1:I.n_nests
     scores(i) = f(nests(i,:));
 end
+
+% Rank the nests in order of fitness
+[temp, sort_indices] =  sort(nests, 'descend');
+abandon_value = floor(I.Pa * I.n_nests); % Nests ranking below this should be discarded
+
+while I.G < 10 % arbitrary stopping parameter
+    % Analyze the eggs within each nest
+    for i = 1:I.n_nests
+        if sort_indices(i) < abandon_value
+            % Abandon the egg
+            abandon(nests(i,:), I)
+        else
+            % Keep the egg
+        end    
+    end
+    
+    I.G = I.G+1;
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Define a fitness/loss function f()
 function [fitness] = f(x)
     fitness = x(1)^2 + 2*x(2)^2 - 0.3*cos(3*pi*x(1)) - 0.4*cos(4*pi*x(2)) + 0.7;
 end
 
+% What to do when the egg is abandoned
+function [newegg, newscore] = abandon(old, I)
+    alpha = ceil(I.A / sqrt(I.G)); % Levy step size
+    newegg = old + levy(alpha, I.n_params, I.B);
+    newscore = f(newegg);
+end
 
 
