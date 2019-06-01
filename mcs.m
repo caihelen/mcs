416a6843eddad3fe613b08f3c493ba0f4e420bd3
caihelen <helen.cai@yale.edu>
2019-06-01 11:40:43 -0600

    nests are diverging?

--- mcs.m
+++ mcs.m
@@ -14,27 +14,26 @@ I.B = 1.1; % Power law index, 1 < B < 2
 % Initialize nests in population
 I.n_nests = 20;
 I.n_params = 2; % = number of positions to optimize
-nests = rand(I.n_nests, I.n_params); % Each row is a nest
+nests_init = rand(I.n_nests, I.n_params); % Each row is a nest
 
 % Calculate all fitness scores in a population
-scores = zeros(I.n_nests, 1);
+scores_init = zeros(I.n_nests, 1);
 for i = 1:I.n_nests
-    scores(i) = f(nests(i,:));
+    scores_init(i) = f(nests_init(i,:));
 end
 
 % Rank the nests in order of fitness
-[temp, sort_indices] =  sort(nests, 'descend');
+nests =  sortrows(horzcat(scores_init, nests_init), 1, 'descend');
 abandon_value = floor(I.Pa * I.n_nests); % Nests ranking below this should be discarded
 
 while I.G < 10 % arbitrary stopping parameter
     % Analyze the eggs within each nest
-    for i = 1:I.n_nests
-        if sort_indices(i) < abandon_value
-            % Abandon the egg
-            abandon(nests(i,:), I)
-        else
-            % Keep the egg
-        end    
+    for i = 1:abandon_value
+        % Abandon the egg and update the new nest values/scores
+        [nests(i,1), nests(i,2), nests(i,3)] = abandon(nests(i,2:I.n_params+1), I)
+    end
+    for i = abandon_value+1:I.n_nests    
+            % Keep the egg   
     end
     
     I.G = I.G+1;
@@ -49,10 +48,18 @@ function [fitness] = f(x)
 end
 
 % What to do when the egg is abandoned
-function [newegg, newscore] = abandon(old, I)
+function [newscore, newegg_1, newegg_2] = abandon(old, I) % fix this return value later
     alpha = ceil(I.A / sqrt(I.G)); % Levy step size
     newegg = old + levy(alpha, I.n_params, I.B);
+    newegg_1 = newegg(1); 
+    newegg_2 = newegg(2); 
     newscore = f(newegg);
+end
+
+% What to do when the egg is kept
+function [newscore, newegg_1, newegg_2] = keep(old, I)
+    % Pick another egg from the top eggs at random
+    
 end
 
 
